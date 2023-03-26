7bb4595a285eefa327e5772484d0020d344fd8f1
joakouu <126687155+joakouu@users.noreply.github.com>
2023-03-26 01:07:41 -0300

    esto es una prueba

--- Partido.java
+++ Partido.java
@@ -1,5 +1,36 @@
 public class Partido {
-    int goles[] = new int[2];
-    String[] equipo = new String[2];
-    int resultado;
+    Equipo equipo1;
+    Equipo equipo2;
+    int golesEquipo1;
+    int golesEquipo2;
+    
+    public ResultadoEnum resultado(Equipo equipo){
+        if(equipo1.equals(equipo)){
+            if(golesEquipo1>golesEquipo2){
+                return ResultadoEnum.ganador;
+            }
+            else if(golesEquipo1<golesEquipo2){
+                return ResultadoEnum.perdedor;
+            }
+            else{
+                return ResultadoEnum.empate;
+            }
+        }
+        else if(equipo2.equals(equipo)){
+            if(golesEquipo2>golesEquipo1){
+                return ResultadoEnum.ganador;
+            }
+            else if(golesEquipo2<golesEquipo1){
+                return ResultadoEnum.perdedor;
+            }
+            else{
+                return ResultadoEnum.empate;
+            }   
+        }
+        return null;
+
+    }
+    
 }
+
+
--- Ronda.java
+++ Ronda.java
@@ -1,3 +1,10 @@
-public class Ronda {
-    int partidos;
+import java.util.ArrayList;
+
+public class Ronda{
+    String num;
+    Partido partido = new Partido();
+    ArrayList<Partido> losPartidos = new ArrayList<Partido>();
+    
 }
+
+
